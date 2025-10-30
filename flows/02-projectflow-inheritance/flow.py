a904a07e481a9d97937c0f1ce4668f85881b97d1
Eddie Mattia <eddie@outerbounds.com>
2025-10-29 17:12:00 -0700

    flow pypi switch

--- flows/02-projectflow-inheritance/flow.py
+++ flows/02-projectflow-inheritance/flow.py
@@ -2,19 +2,21 @@ from metaflow import step, pypi
 from obproject import ProjectFlow
 from src.flow_templates import NeuralNetworkFlow
 
+### Uncomment @pypi for remote execution (Kubernetes/Batch).
+pypi = pypi(packages={"min-obproject": "0.1.3"}) # remote   
 
-# Uncomment @pypi_base for remote execution (Kubernetes/Batch).
-# Comment out for local development (use `uv pip install -e .` instead).
-# @pypi_base(packages={"min-obproject": ""})
+### Uncomment out local development (with `uv pip install -e .`).
+# pypi = pypi(disabled=True) # local 
+
 class CustomizedTrainingFlow(ProjectFlow, NeuralNetworkFlow):
 
-    @pypi(packages={"min-obproject": "0.1.3"})
+    @pypi
     @step
     def start(self):
         self._resolve_nn_config()  # via NeuralNetworkFlow.
         self.next(self.end)
 
-    @pypi(packages={"min-obproject": "0.1.3"})
+    @pypi
     @step
     def end(self):
         # stdout should show your override value and another default value.
